5a23bc015a439c4b1840dfcefecc79f8f5d997c8
Nynra <61116869+Nynra@users.noreply.github.com>
2023-03-12 09:49:38 +0100

    added debian setup file

--- docker_setup_debian.bash
+++ docker_setup_debian.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This file contains the commands to install Docker on Ubuntu 20.04
+# This file contains the commands to install Docker on Debian buster
 # source: https://docs.docker.com/engine/install/ubuntu/
 # Remove the existing installation
 sudo apt-get remove docker docker-engine docker.io containerd runc
@@ -14,11 +14,11 @@ sudo apt-get install \
 
 # Add Docker’s official GPG key
 sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 # Set up the stable repository
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install Docker Engine
@@ -28,10 +28,6 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 # Add the current user to the docker group
 sudo groupadd docker
 sudo usermod -aG docker $USER
-
-# Install Docker Compose
-sudo apt-get install docker-compose-plugin
-docker compose version
 
 # Test the installation
 docker run hello-world
