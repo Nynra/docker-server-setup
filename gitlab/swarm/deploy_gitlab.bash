4763197c7b39584b67c0833c475e4380a583197b
Nynra <61116869+Nynra@users.noreply.github.com>
2023-03-13 21:37:11 +0100

    added volumes to the stack file

--- gitlab/swarm/deploy_gitlab.bash
+++ gitlab/swarm/deploy_gitlab.bash
@@ -4,6 +4,6 @@ echo "external_url 'https://my.domain.com/'" > gitlab.rb
 echo "gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password').gsub("\n", "")" >> gitlab.rb
 
 read -p "Enter the GitLab root password: " -s GITLAB_ROOT_PASSWORD
-echo $GITLAB_ROOT_PASSWORD > gitlab_root_password
+echo $GITLAB_ROOT_PASSWORD > root_password.txt
 
 docker stack deploy --compose-file docker-compose.yml gitlab
